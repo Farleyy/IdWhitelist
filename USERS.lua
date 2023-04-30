3bcc327314b8b121055a14e597ac13d806d18125
Farleyy <94583502+Farleyy@users.noreply.github.com>
2023-04-29 21:17:24 -0500

    Update USERS.lua

--- USERS.lua
+++ USERS.lua
@@ -1,8 +1,7 @@
 local whitelisted = {
-    [3836700365] = true,
+    	[3836700365] = true,
 	[3150825414] = true,
-	[3718625702] = true
+	[3718625702] = true,
+	[3421441767] = true
 }
-
-
 return whitelisted
